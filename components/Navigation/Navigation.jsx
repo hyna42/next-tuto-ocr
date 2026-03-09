b02d7fd28c9ab63cac2501eb2e8951c9797828e4
hyna42 <bylo@duck.com>
2026-03-09 01:47:15 +0100

    fix(style): nav

--- components/Navigation/Navigation.jsx
+++ components/Navigation/Navigation.jsx
@@ -1,37 +1,45 @@
+"use client"
+import {usePathname} from 'next/navigation'
 import Link from 'next/link'
 import styles from './Navigation.module.css'
 
 export default function Navigation() {
- return (
-   <nav className={styles.nav}>
-     <div className="container">
-       <Link href="/" className={styles.logo}>
-         Mon Portfolio
-       </Link>
+    const pathname = usePathname()
+  return (
+    <nav className={styles.nav}>
+      <div className="container">
+        <Link href="/" className={styles.logo}>
+          Mon Portfolio
+        </Link>
 
-       <ul className={styles.menu}>
-         <li>
-           <Link href="/" className={styles.link}>
-             Accueil
-           </Link>
-         </li>
-         <li>
-           <Link href="/projets" className={styles.link}>
-             Projets
-           </Link>
-         </li>
-         <li>
-           <Link href="/a-propos" className={styles.link}>
-             À propos
-           </Link>
-         </li>
-         <li>
-           <Link href="/contact" className={styles.link}>
-             Contact
-           </Link>
-         </li>
-       </ul>
-     </div>
-   </nav>
- )
+        <ul className={styles.menu}>
+          <li>
+            <Link href="/" className={pathname === '/' ? `${styles.link} ${styles.active}` : styles.link}>
+              Accueil
+            </Link>
+          </li>
+          <li>
+            <Link href="/projets" className={pathname === '/projets' ? `${styles.link} ${styles.active}` : styles.link}>
+              Projets
+            </Link>
+          </li>
+          {/* <li>
+            <Link href="/formation" className={pathname === '/formation' ? `${styles.link} ${styles.active}` : styles.link}>
+              Formations
+            </Link>
+          </li> */}
+          <li>
+            <Link href="/a-propos" className={pathname === '/a-propos' ? `${styles.link} ${styles.active}` : styles.link}>
+              À propos
+            </Link>
+          </li>
+          <li>
+            <Link href="/contact" className={pathname === '/contact' ? `${styles.link} ${styles.active}` : styles.link}>
+              Contact
+            </Link>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  )
 }
